8e02cb5d3d92d8ffe4b754caa46c05d4a6fdb9b1
Naoki Mizukami <mizukami@ucar.edu>
2019-10-06 11:03:25 -0600

    added setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pfaf_decode
-python script to decode pfafstetter code
+python script to decode pfafstetter code to identify river network relationships
+
+1. upstream/downstream relationship
+2. immediate downstream reach
+
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+import sys
+from setuptools import setup, find_packages
+# import versioneer
+
+# with open("requirements.txt") as f:
+#     INSTALL_REQUIRES = f.read().strip().split("\n")
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+PYTHON_REQUIRES = '>=3.6'
+
+description = ("module to decode pfafstetter code")
+setup(
+    name="pfaf_decoder",
+    description=description,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    maintainer="Naoki Mizukami",
+    maintainer_email="mizukami@ucar.edu",
+    url="https://github.com/mizukami/pfaf_decode",
+    py_modules=['pfaf_decoder'],
+    packages=find_packages(),
+    python_requires=PYTHON_REQUIRES,
+    license="Apache",
+    keywords="pfafstetter",
+    version='0.0.1',
+)
